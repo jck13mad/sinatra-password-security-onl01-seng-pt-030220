2764625d4dc00c25afa8e91aa2d7310917a4aee1
Jack Madden <jck13mad@gmail.com>
2020-06-11 00:34:23 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,7 @@ class ApplicationController < Sinatra::Base
 
 	post "/signup" do
 		#your code here!
-		
+		user = User.new(:username => params[:username], :password => params[:password])
 	end
 
 	get "/login" do
@@ -27,6 +27,7 @@ class ApplicationController < Sinatra::Base
 
 	post "/login" do
 		#your code here!
+		
 	end
 
 	get "/success" do
